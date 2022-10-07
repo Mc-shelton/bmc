068d6303996bae57d6d1ec0538dfda04c0a27160
Mc-shelton <78601096+Mc-shelton@users.noreply.github.com>
2022-10-07 17:10:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const refresh = ()=>{
+    const incid = document.getElementsByClassName('trimTable')
+    const button = document.getElementById('WIN_0_304352241')
 
+    var toggle = 1;
+    var interv;
+    incid[15].addEventListener('click', ()=>{
+        if(toggle == 1){
+            toggle = 0
+            alert("Auto refresh started")
+    interv = setInterval(()=>{
+        button.click();
+    },1500)
+        }else{
+            toggle = 1;
+            alert("Auto refresh closed")
+            clearInterval(interv)
+        }})}
+            
